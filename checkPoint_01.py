35a37ca459cee6a9909b2e36ab4bdd4fba808321
White Ugly <byl151462@gmail.com>
2018-11-11 00:27:12 +0800

    My first checkpoint:)

---
+++ checkPoint_01.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://pvp.qq.com/web201605/herodetail/199.shtml'
+r = requests.get(url)
+r.encoding = r.apparent_encoding
+demo = r.text
+
+soup = BeautifulSoup(demo, 'html.parser')
+
+skillname = []
+skilldesc = []
+for x in soup.find_all('p', 'skill-name'):
+    skillname.append(x.b.string)
+for x in soup.find_all('p', 'skill-desc'):
+    skilldesc.append(x.string)
+
+for i in range(len(skillname)-1):
+    print(skillname[i])
+    print(skilldesc[i])
+    print()
