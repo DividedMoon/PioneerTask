778256e0d303053219ebfdb648531d91b8b1312c
White Ugly <byl151462@gmail.com>
2018-11-24 21:29:17 +0800

    Update the data

--- checkPoint_03.py
+++ checkPoint_03.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import request, render_template
 import json
 import urllib.request
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template('true-index.html')
 
 
 user = 'inspector'
@@ -18,11 +20,11 @@ des1 = 'But I always failed.'
 des2 = 'So here it is.:)'
 
 
-@app.route('/username', methods=('GET', 'POST'))
+@app.route('/item', methods=('GET', 'POST'))
 def test():
     global user, item_total_price, item_price, item_name, des1, des2
     if request.method == 'POST':
-        user = request.values.get("username")
+        user = request.values.get("item_name")
 
     url = 'https://pvp.qq.com/web201605/js/item.json'
     res = urllib.request.urlopen(url)
@@ -42,6 +44,44 @@ def test():
                            item_total_price=item_total_price,
                            des1=des1,
                            des2=des2)
+
+
+hero_title = 'None'
+hero_name = 'None'
+hero_ename = 'None'
+
+
+@app.route('/hero', methods=('GET', 'POST'))
+def hero():
+    global hero_title, hero_name, hero_ename
+    if request.method == 'POST':
+        hero_name = request.values.get("hero_name")
+
+    url = 'https://pvp.qq.com/web201605/js/herolist.json'
+    res = urllib.request.urlopen(url)
+    hero_json = json.loads(res.read())
+    for i in hero_json:
+        if i['cname'] == hero_name:
+            hero_title = i['title']
+            hero_ename = i['ename']
+    hero_number = str(hero_ename)
+    anurl = 'https://pvp.qq.com/web201605/herodetail/' + hero_number + '.shtml'
+    r = requests.get(anurl)
+    r.encoding = r.apparent_encoding
+    demo = r.text
+    soup = BeautifulSoup(demo, 'html.parser')
+    skill_name = []
+    skill_desc = []
+    for x in soup.find_all('p', 'skill-name'):
+        skill_name.append(x.b.string)
+    for x in soup.find_all('p', 'skill-desc'):
+        skill_desc.append(x.string)
+    return render_template('index.html',
+                           hero_id=hero_number,
+                           hero_name=hero_name,
+                           hero_title=hero_title,
+                           skill_name=skill_name,
+                           skill_desc=skill_desc)
 
 
 if __name__ == '__main__':
